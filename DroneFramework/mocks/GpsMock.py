eea79a82f1e8ba2c9f57dcd6c124e19f4ea35712
Georg <jorge.encinas@gmail.com>
2016-04-21 15:07:29 -0400

    iniciando el gps

--- DroneFramework/mocks/GpsMock.py
+++ DroneFramework/mocks/GpsMock.py
@@ -1,1 +1,7 @@
 import mock.py
+
+def test():
+  from mock import MagicMock
+  sensorGPS = MagicMock(return_value=['Altitud: 100','Longitud: 50'])
+  print sensorGPS.return_value
+  sensorGPS(return_altitude='35')
